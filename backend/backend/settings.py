92d975bd6a35ec246cedbdb68009fcd7a3a6b948
seiviel14 <senriquevelasquez@gmail.com>
2022-07-18 20:59:41 +0200

    Default authentication class added to the rest framework segment of the project settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,9 +76,12 @@ CORS_ORIGIN_WHITELIST = [
 AUTH_USER_MODEL = 'core_user.User'
 
 REST_FRAMEWORK = {
-     'DEFAULT_RENDERER_CLASSES': (
-         'rest_framework.renderers.JSONRenderer',
-     )
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
  }
 
 ROOT_URLCONF = 'backend.urls'
