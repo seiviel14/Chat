c964e466b974f1723248bd5d078c37cc1c09fc6e
seiviel14 <senriquevelasquez@gmail.com>
2022-07-21 23:57:18 +0200

    Register Serializer created in the Core.Auth app

--- backend/core/auth/serializers.py
+++ backend/core/auth/serializers.py
@@ -40,3 +40,18 @@ class LoginSerializer(TokenObtainPairSerializer):
             update_last_login(None, self.user)
 
         return data
+
+class RegisterSerializer(UserSerializer):
+    password = serializers.CharField(max_length=128, min_length=8, write_only=True, required=True)
+    email = serializers.EmailField(required=True, write_only=True, max_length=128)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'is_active', 'created', 'updated']
+
+    def create(self, validated_data):
+        try:
+            user = User.objects.get(email=validated_data['email'])
+        except ObjectDoesNotExist:
+            user = User.objects.create_user(**validated_data)
+        return user
