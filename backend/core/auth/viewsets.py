ef04968ea82e8aecca249872ce43f6384bef7953
seiviel14 <senriquevelasquez@gmail.com>
2022-07-25 23:01:09 +0200

    Refresh Viewset created for the Core.Auth app

--- backend/core/auth/viewsets.py
+++ backend/core/auth/viewsets.py
@@ -1,11 +1,13 @@
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import viewsets
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
-from core.auth.serializers import LoginSerializer, RegistrationSerializer
+from rest_framework_simplejwt.views import TokenRefreshView
+from core.auth.serializers import LoginSerializer, RegisterSerializer
 
 class LoginViewSet(ModelViewSet, TokenObtainPairView):
     serializer_class = LoginSerializer
@@ -43,3 +45,17 @@ class RegistrationViewSet(ModelViewSet, TokenObtainPairView):
             "refresh": res["refresh"],
             "token": res["access"]
         }, status=status.HTTP_201_CREATED)
+
+class RefreshViewSet(viewsets.ViewSet, TokenRefreshView):
+    permission_classes = (AllowAny,)
+    http_method_names = ['post']
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except TokenError as e:
+            raise InvalidToken(e.args[0])
+
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
