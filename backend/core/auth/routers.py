e955be06656dcd226c2653bf5e9ae06d38298fcc
seiviel14 <senriquevelasquez@gmail.com>
2022-07-25 23:07:51 +0200

    Routers created for Core.Auth and Core.User apps viewsets

---
+++ backend/core/auth/routers.py
@@ -0,0 +1,15 @@
+from nturl2path import url2pathname
+from rest_framework.routers import SimpleRouter
+from core.user.viewsets import UserViewSet
+from core.auth.viewsets import LoginViewSet, RegistrationViewSet, RefreshViewSet
+
+routes = SimpleRouter()
+
+routes.register('auth/login', LoginViewSet, basename='login')
+routes.register('auth/register', RegistrationViewSet, basename='register')
+routes.register('auth/refresh', RefreshViewSet, basename='refresh')
+routes.register('user', UserViewSet, basename='user')
+
+urlpatterns = [
+    *routes.urls
+]
